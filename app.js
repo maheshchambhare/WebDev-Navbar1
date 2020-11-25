301630ad9bca603a813b882f58871039d5643e8e
mahesh <chambharem7@gmail.com>
2020-11-25 21:09:42 +0530

    Navbar1

---
+++ app.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#mobile_menu');
+const menulinks = document.querySelector('.navbar_menu');
+
+
+const mobilemenu = () => {
+    menu.classList.toggle('is-active');
+    menulinks.classList.toggle('active');
+};
+
+menu.addEventListener('click',mobilemenu);
